51a7df6fbcb4e1954096a570e9b3fabd97aad7b0
Dan <dan.usa@spatchcock.org>
2013-01-06 12:04:10 -0600

    simple lookahead for the follow cam

--- Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
+++ Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
@@ -18,10 +18,15 @@ var height = 5.0;
 // How much we 
 var heightDamping = 2.0;
 var rotationDamping = 3.0;
+var lastLookahead : Vector3;
 
 // Place the script in the Camera-Control group in the component menu
 @script AddComponentMenu("Camera-Control/Smooth Follow")
 
+function Start()
+{
+	lastLookahead = Vector3.fwd; 
+}
 
 function LateUpdate () 
 {
@@ -29,30 +34,31 @@ function LateUpdate ()
 	if (!target)
 		return;
 	
-	// Calculate the current rotation angles
-	var wantedRotationAngle = target.eulerAngles.y;
-	var wantedHeight = target.position.y + height;
-		
-	var currentRotationAngle = transform.eulerAngles.y;
+	// Calculate the current height
+	var wantedHeight = target.position.y + height;		
 	var currentHeight = transform.position.y;
 	
-	// Damp the rotation around the y-axis
-	currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-
 	// Damp the height
 	currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
 	// Convert the angle into a rotation
-	var currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+	var currentRotation = Quaternion.Euler (0, 180, 0);
 	
 	// Set the position of the camera on the x-z plane to:
 	// distance meters behind the target
 	transform.position = target.position;
 	transform.position -= currentRotation * Vector3.forward * distance;
 
-	// Set the height of the camera
+	// Set the height of the cameras
 	transform.position.y = currentHeight;
 	
 	// Always look at the target
 	transform.LookAt (target);
+	
+	// add in some rudimentary lookahead
+	var lookAhead : Vector3 = Vector3.Lerp(lastLookahead, target.transform.forward * 2, 0.01);
+	lookAhead.y = 0;
+	transform.position = Vector3.Lerp(transform.position, transform.position - lookAhead, 1);
+	
+	lastLookahead = lookAhead;
 }
